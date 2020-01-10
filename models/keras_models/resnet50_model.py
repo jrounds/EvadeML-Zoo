998bbe2184e51e85427bec8871579c182f842560
jrounds <roundsjeremiah@gmail.com>
2020-01-09 17:09:57 -0800

    more keras 2.2.2 changes

--- models/keras_models/resnet50_model.py
+++ models/keras_models/resnet50_model.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 
 import tensorflow as tf
 from utils import load_externals
-from resnet50 import *
+from keras.applications.resnet50 import *
 
 from keras.applications.imagenet_utils import _obtain_input_shape
 from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
