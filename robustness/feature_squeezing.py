f5b1316b71e4e20a2090dda7d3fa31bfbf41fad2
jrounds <roundsjeremiah@gmail.com>
2020-01-14 09:24:48 -0800

    python 3 has_key

--- robustness/feature_squeezing.py
+++ robustness/feature_squeezing.py
@@ -16,9 +16,9 @@ class FeatureSqueezingRC:
         subject, params = parse_params(rc_name)
         assert subject == 'FeatureSqueezing'
 
-        if params.has_key('squeezer'):
+        if "squeezer" in params: #params.has_key('squeezer'):
             self.filter = get_squeezer_by_name(params['squeezer'], 'python')
-        elif params.has_key('squeezers'):
+        elif "squeezers" in params: #params.has_key('squeezers'):
             squeezer_names = params['squeezers'].split(',')
             self.filters = [ get_squeezer_by_name(squeezer_name, 'python') for squeezer_name in squeezer_names ]
 
